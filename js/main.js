6be08253c85d4794ca8c218059d408d259cbe4af
Mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-09-02 18:08:30 +0500

    Add chats sitebar modal

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
 const LOCAL_PASSWORD = 'admin';
 
+// SIDEBAR MODAL
+let elsSidebarModalProfilesToggler = document.querySelectorAll('.js-sidebar-modal-profiles-toggler');
+let elSidebarModalProfilesIcon = document.querySelector('.sidebar-modal__toggle-icon');
+
+// LOCK MODAL
 let elLockModal = document.querySelector('.lock-modal');
 let elLockModalForm = elLockModal.querySelector('.js-lock-modal-form');
 let elLockModalPassword = elLockModalForm.querySelector('.js-lock-modal-password-input');   
 let elLockModalIncorrectPasswordNotice = elLockModalForm.querySelector('.js-lock-modal-incorrect-password-notice');
 
-elLockModalForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
 
-  let userPassword = elLockModalPassword.value;
+// SIDEMAR MODAL
+if(elsSidebarModalProfilesToggler.length > 0) {
+  elsSidebarModalProfilesToggler.forEach(function (toggler) {
+    toggler.addEventListener('click', function() {
+      elSidebarModalProfilesIcon.classList.toggle('sidebar-modal__toggle-icon--open');
+    }); 
+  });
 
-  if (userPassword === LOCAL_PASSWORD) {
-    $('#lock-modal').modal('hide');
-    elLockModalIncorrectPasswordNotice.classList.add('d-none');
-    elLockModalPassword.value = '';
-  } else {
-    elLockModalIncorrectPasswordNotice.classList.remove('d-none');
-  }
+}
 
-});
+
+// LOCK MODAL
+if(elLockModalForm) {
+  elLockModalForm.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+  
+    let userPassword = elLockModalPassword.value;
+  
+    if (userPassword === LOCAL_PASSWORD) {
+      $('#lock-modal').modal('hide');
+      elLockModalIncorrectPasswordNotice.classList.add('d-none');
+      elLockModalPassword.value = '';
+    } else {
+      elLockModalIncorrectPasswordNotice.classList.remove('d-none');
+    }
+  
+  });
+}
